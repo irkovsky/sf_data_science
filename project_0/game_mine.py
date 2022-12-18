24f988086d645c63f58fa088cbce565e283f1448
irkovsky <irkovsky@gmail.com>
2022-12-18 21:21:39 +0300

    added game_mine.py

---
+++ project_0/game_mine.py
@@ -0,0 +1,70 @@
+''' Guess the number game. Computer takes a number and tries to guess it itself within 20 tries'''
+
+import numpy as np
+
+number = np.random.randint(1,101) 
+print(f'Компьютер загадал число: {number}')
+
+
+def start_guess_counter(number:int): 
+    '''Method of guessing a number. 
+    First it sets counter of guesses to zero and returns "guess_number" function.
+    
+    Args
+        number(int): the number computer should guess
+    
+    Returns: function which makes guesses and counts tries itself
+    '''
+    count = 0
+
+    def guess_number(number:int=1, start:int=1, end:int=101)-> int:
+        '''Method of guessing itself.
+        Computer makes guesses and counts tries until it's succesful.
+        
+        Args:
+            number(int): the number computer should guess
+            start: min number of the random range for guessing
+            end: max number of the random range for guessing
+        
+        Returns:
+            count(int, nonlocal): counter for number of guesses.
+        '''
+        guess_try = np.random.randint(start, end) 
+        
+        nonlocal count
+        count += 1 
+        
+        if guess_try == number: return count 
+        
+        if guess_try > number: end=guess_try        
+        elif guess_try < number: start=guess_try
+            
+        return guess_number(number, start, end)
+    
+    return guess_number(number)
+
+
+guess_number = start_guess_counter
+              
+print(f'{guess_number(number)} - количество попыток компьютера угадать число')
+
+
+def average_result(guess_number) -> int:
+    '''Determines an average number of tries the method needs to guess the number.
+    Uses the array of 1000 numbers passed to the method.
+    
+    Args:
+        guess_number: name of guessing method
+    
+    Returns:
+        int: average number of guesses
+    '''
+    
+    guesses_array = list(np.random.randint(1,101,size=1000))
+    
+    results=list(map(guess_number, guesses_array))
+    
+    return int(np.mean(results))
+
+
+print(f'Алгоритм угадывает число в среднем за {average_result(guess_number)} попыток/-ки')
