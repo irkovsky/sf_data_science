d82d9c0b92c8c4ce0de26521ed3bc75409195dcd
irkovsky <irkovsky@gmail.com>
2023-01-29 00:43:50 +0300

    alles at once

--- Blok 9/unit1_collect.py
+++
@@ -1,9 +0,0 @@
-from collections import Counter
-
-c = Counter()
-print(type(c)) # <class 'collections.Counter'>
-print(c) # Counter()
-
-cars = ['red', 'blue', 'black', 'black', 'black', 'red', 'blue', 'red', 'white']
-c = Counter(cars)
-print(c)
